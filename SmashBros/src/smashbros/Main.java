496f5828e38411234511d721a7f592e33624068d
Johannes <jojo.warnke@gmail.com>
2018-05-15 08:59:22 +0200

    initial commit

---
+++ SmashBros/src/smashbros/Main.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package smashbros;
+
+/**
+ *
+ * @author joh.warnke
+ */
+public class Main {
+    
+}
