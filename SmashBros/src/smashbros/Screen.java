213752a363a5519925975cda35fbd153dd31a1d8
Johannes <jojo.warnke@gmail.com>
2018-06-12 08:50:37 +0200

    Update Screen.java

--- SmashBros/src/smashbros/Screen.java
+++ SmashBros/src/smashbros/Screen.java
@@ -7,20 +7,35 @@
 package smashbros;
 import javax.swing.JFrame;
 import java.awt.Dimension;
+import java.awt.Canvas;
+import java.awt.Graphics;
 /**
  *
  * @author joh.warnke
  */
 public class Screen{
-    
+    JFrame f;
+    Drawer d;
+    Spiel s;
     public Screen(int width, int height, String name, String bgimg) {
-    JFrame f = new JFrame(name);
+    f = new JFrame(name);
+    d = new Drawer();
     f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     f.setSize(new Dimension(width, height));
+    f.add(d);
     f.setLocation(50,50);
     f.setVisible(true);
+    s = new Spiel(f);
     }
     public static void main (String[] args) {
         new Screen(500,400,"test","test");
     }       
+    
+    public class Drawer extends Canvas {
+        
+        @Override
+        public void paint(Graphics g){
+            s.draw(g);
+        }
+    }
 }
