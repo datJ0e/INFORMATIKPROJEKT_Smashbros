d2d10e9c7fd648d83da0506fb8a11413bc955ee3
Johannes <jojo.warnke@gmail.com>
2018-06-26 09:22:22 +0200

    Update Main.java

--- SmashBros/src/smashbros/Main.java
+++ SmashBros/src/smashbros/Main.java
@@ -11,5 +11,12 @@ package smashbros;
  * @author joh.warnke
  */
 public class Main {
+    public static ScreenSwitch sw = new ScreenSwitch();
+    public static void main (String[] args) {
+        new Screen(450,300,"SmashBros","login");
+    } 
     
+    public static ScreenSwitch getScreenSwitch() {
+    	return sw;
+    }
 }
