d4a157d64549e92d31a36f0aa5d9b4aeeb770d3f
Johannes <jojo.warnke@gmail.com>
2018-06-29 08:55:04 +0200

    julian_files

--- SmashBros/src/smashbros/DBHelper.java
+++ SmashBros/src/smashbros/DBHelper.java
@@ -1,36 +1,49 @@
-package smashbros;
-
 import java.sql.*;
+import java.text.DateFormat;
+import java.util.GregorianCalendar;
 public class DBHelper {
 	static Connection c = null;
     static Statement stmt = null;
 	public static void main(String[] args) { 
-	      //db_tabellenErstellen();
+	      db_tabellenErstellen();
 		  benutzerdatenAusgeben();
-		  neuenBenutzerAnlegen("Ich", "123");
-		  neuenBenutzerAnlegen("Du", "1234");
-		  neuenBenutzerAnlegen("Er", "12345");
+		//  neuenBenutzerAnlegen("Ich", "123");
+		  //neuenBenutzerAnlegen("Du", "1234");
+		  //neuenBenutzerAnlegen("Er", "12345");
+		 // db1UpdateSpielerdaten("Er", 4,5);
+		  //neuesSpielAnlegen("Er","Ich","1:5",100,10,aktuellesDatum());
+		 // neuesSpielAnlegen("Du","Ich","1:5",100,10,aktuellesDatum());
+		  //neuesSpielAnlegen("Er","Ich","10:5",100,10,aktuellesDatum());
+		  //neuesSpielAnlegen("Er","Ich","1:299",100,10,aktuellesDatum());
+		  spielergebnisseAusgeben("Ich");
 	}
+	//Gibt das aktuelle Datum zurück
+	static String aktuellesDatum() {
+		GregorianCalendar now = new GregorianCalendar(); 
+		DateFormat df = DateFormat.getDateInstance(DateFormat.MEDIUM);
+		String date=df.format(now.getTime());
+        return date;
+    } 
+	//Erstellt die Tabellen (wenn noch nicht vorhanden)
 	public static void db_tabellenErstellen() {
 		try {
 	         Class.forName("org.sqlite.JDBC");
 	         c = DriverManager.getConnection("jdbc:sqlite:test.db");
 	         System.out.println("Opened database successfully");
-
 	         stmt = c.createStatement();
-	         String sql1="CREATE TABLE spielername_passwort_highscore" +
+	         String sql1="CREATE TABLE IF NOT EXISTS 'spielername_passwort_highscore' "+
 	                    "(spielername	TEXT NOT NULL UNIQUE, "+
 	        			"Passwort	    TEXT NOT NULL, "+
 	        			"pershighscore	INTEGER DEFAULT 0,"+
 	        			"highscoreSpielId	INTEGER )";
-	         String sql2="CREATE TABLE spielergebnisse"+ 
+	         String sql2="CREATE TABLE IF NOT EXISTS 'spielergebnisse' "+ 
 	        		 	"(Spieler1 TEXT NOT NULL,"+
 	        		 	"Spieler2 TEXT NOT NULL,"+
 	        		 	"Ergebnis TEXT," 
 	        		 	+"Spiel_ID INTEGER NOT NULL UNIQUE," 
 	        		 	+"LocalScoreS1 INTEGER," 
 	        		 	+"LocalScoreS2 INTEGER,"
-	        		 	+"Datum DATE NOT NULL )";
+	        		 	+"Datum TEXT NOT NULL )";
 	         stmt.executeUpdate(sql1);
 	         stmt.executeUpdate(sql2);
 	         stmt.close();
@@ -40,23 +53,86 @@ public class DBHelper {
 	      }
 	      System.out.println("Table created successfully");
 	}
-	public static boolean neuesSpielAnlegen(String s1,String s2,String Ergebnis,int Spiel_ID,int lsc1,int lsc2,Date date) {
+	//Erstellt eine Liste aller Ergebnisse des angegebenen Spielers
+	public static void spielergebnisseAusgeben(String s) {
 		try {
+			Spielergebnisse.spErList.clear();
+			c = DriverManager.getConnection("jdbc:sqlite:test.db");
+			stmt = c.createStatement();
+			ResultSet rs=stmt.executeQuery("SELECT * From spielergebnisse WHERE Spieler1='"+s+"'or Spieler2='"+s+"'");
+			while(rs.next()) {
+				String s1=rs.getString("Spieler1");
+				String s2=rs.getString("Spieler2");
+				String erg=rs.getString("Ergebnis");
+				int spiel_ID=rs.getInt("Spiel_ID");
+				int lsc1=rs.getInt("LocalScoreS1");
+				int lsc2=rs.getInt("LocalScoreS2");
+				String date=rs.getString("Datum");
+				Spielergebnisse.spErList.add(new Spielergebnisse(s1, s2, erg, lsc1, lsc2, spiel_ID, date));
+				System.out.println(s1+"-"+s2+"-"+erg+"-"+Integer.toString(spiel_ID)+"-"+Integer.toString(lsc1)+"-"+Integer.toString(lsc2)+"-"+date);
+			}
+			rs.close();
+			stmt.close();
+			c.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		 System.out.println("Opened database successfully");
+	}
+	//generiert eine einzigartige SpielID
+	public static int SpielIDgenerieren() {
+		try {
+			c = DriverManager.getConnection("jdbc:sqlite:test.db");
+			stmt = c.createStatement();
+			ResultSet rs=stmt.executeQuery("SELECT Spiel_ID From spielergebnisse Order By Spiel_ID DESC");
+			if(rs.isBeforeFirst()) {
+				rs.getInt(1); 
+				int id=rs.getInt("Spiel_ID")+1;
+				rs.close();
+				stmt.close();
+				c.close();
+				return id;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	//die Spielerdaten werden im Profil des Spielers aktuallisiert
+	public static boolean db1UpdateSpielerdaten(String s,int lsc,int spiel_ID) {
+		try {
+			c = DriverManager.getConnection("jdbc:sqlite:test.db");
+			stmt = c.createStatement();
+			stmt.executeUpdate("Update spielername_passwort_highscore SET pershighscore= "+Integer.toString(lsc)+","
+					+ " highscoreSpielId="+Integer.toString(spiel_ID)+" WHERE spielername='"+s+"';");
+					stmt.close();
+					c.close();
+					return false;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	//ein neues Spiel wird in die Datenbank eingetragen
+	public static boolean neuesSpielAnlegen(String s1,String s2,String Ergebnis,int lsc1,int lsc2,String date) {
+		try {
+		int i=SpielIDgenerieren();
 		c = DriverManager.getConnection("jdbc:sqlite:test.db");
-		System.out.println("Opened database successfully");
-		String query="INSERT Into spielergebnisse(Spieler1,Spieler2,Ergebnis,Spiel_ID,LocalScoreS1,LocalScoreS2,Datum DATE) + "
-				+ "values(?,?,?,?)";
+		String query="INSERT Into spielergebnisse(Spieler1,Spieler2,Ergebnis,Spiel_ID,LocalScoreS1,LocalScoreS2,Datum) values(?,?,?,?,?,?,?)";
 		PreparedStatement pst=c.prepareStatement(query);
 		pst.setString(1, s1);
 		pst.setString(2, s2);
 		pst.setString(3, Ergebnis);
-		pst.setInt(4, Spiel_ID);
+		pst.setInt(4, i);
 		pst.setInt(5, lsc1);
 		pst.setInt(6, lsc2);
-		pst.setDate(7, date);
+		pst.setString(7, date);
 		pst.execute();
 		pst.close();
 		c.close();
+		DBHelper.db1UpdateSpielerdaten(s1,lsc1,i);
+		DBHelper.db1UpdateSpielerdaten(s2,lsc2,i);
 		return true;
 		}catch(SQLException e) {
 			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
@@ -64,11 +140,11 @@ public class DBHelper {
 		}
 		return false;
 	}
+	//ein neuer Spieler wird in die Datenbak eingetragen
 	public static boolean neuenBenutzerAnlegen(String name,String pw) {
 		try {
-			if(prÃ¼feNamen(name)) {
+			if(prüfeNamen(name)) {
 				c = DriverManager.getConnection("jdbc:sqlite:test.db");
-				System.out.println("Opened database successfully");
 				String query="INSERT Into spielername_passwort_highscore(spielername,Passwort,pershighscore,highscoreSpielId) "
 						+ "values(?,?,?,?)";
 				PreparedStatement pst=c.prepareStatement(query);
@@ -82,7 +158,7 @@ public class DBHelper {
 				System.out.println("Spieler erfolgreich angelegt");
 				return true;
 			}else {
-				System.out.println("Spieler nicht angelegt ! Bitte Namen Ã¤ndern");
+				System.out.println("Spieler nicht angelegt ! Bitte Namen ändern");
 			}
 			return false;
 		} catch (SQLException e) {
@@ -91,7 +167,8 @@ public class DBHelper {
 		}
 		return false;
 	}
-	private static boolean prÃ¼feNamen(String name) {
+	//prüft obder Name zulässig ist
+	private static boolean prüfeNamen(String name) {
 		try {
 			c = DriverManager.getConnection("jdbc:sqlite:test.db");
 			System.out.println("Opened database successfully");
@@ -137,7 +214,8 @@ public class DBHelper {
 		 System.out.println("Opened database successfully");
 	
 	}
-	public static boolean prÃ¼feAnmeldedaten(String name,String pw) {
+	//Anmeldedaten werden überprüft
+	public static boolean prüfeAnmeldedaten(String name,String pw) {
 		try {
 			c = DriverManager.getConnection("jdbc:sqlite:test.db");
 			System.out.println("Opened database successfully");
@@ -164,7 +242,6 @@ public class DBHelper {
 			stmt.close();
 			c.close();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return false;
---
+++ SmashBros/src/smashbros/Spielergebnisse.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class Spielergebnisse {
+	 static String s1,s2,ergebnis;
+	static int lsc1,lsc2,spiel_ID;
+	static String date;
+	static ArrayList<Spielergebnisse> spErList=new ArrayList<>();
+	public Spielergebnisse(String s1, String s2, String ergebnis, int lsc1, int lsc2, int spiel_ID, String date) {
+		super();
+		Spielergebnisse.s1 = s1;
+		Spielergebnisse.s2 = s2;
+		Spielergebnisse.ergebnis = ergebnis;
+		Spielergebnisse.lsc1 = lsc1;
+		Spielergebnisse.lsc2 = lsc2;
+		Spielergebnisse.spiel_ID = spiel_ID;
+		Spielergebnisse.date = date;
+	}
+	public static String getS1() {
+		return s1;
+	}
+	public static String getS2() {
+		return s2;
+	}
+	public static String getErgebnis() {
+		return ergebnis;
+	}
+	public static int getLsc1() {
+		return lsc1;
+	}
+	public static int getLsc2() {
+		return lsc2;
+	}
+	public static int getSpiel_ID() {
+		return spiel_ID;
+	}
+	public static String getDate() {
+		return date;
+	}
+	
+}
